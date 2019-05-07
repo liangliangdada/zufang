26a797c224d75b9d1dee256fa7e4577a58089751
e <1216569087@qq.com>
2019-05-07 13:28:01 +0800

    整合mybatis-plus + jpa + druid

---
+++ src/main/java/com/liang/zufang/entity/BaseEntity.java
@@ -0,0 +1,29 @@
+package com.liang.zufang.entity;
+
+/**
+ * @author liuliang
+ * @description 顶级父类
+ * @date 2019/5/7
+ **/
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import java.io.Serializable;
+
+@MappedSuperclass
+public class BaseEntity implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
---
+++ src/main/java/com/liang/zufang/entity/sys/User.java
@@ -0,0 +1,61 @@
+package com.liang.zufang.entity.sys;
+
+import com.liang.zufang.entity.BaseEntity;
+
+import javax.naming.Name;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * @author liuliang
+ * @description 用户
+ * @date 2019/5/7
+ **/
+@Entity
+@Table(name = "sys_user")
+public class User extends BaseEntity {
+
+    @Column(length = 100)
+    private String name;
+
+    @Column(length = 100)
+    private String userName;
+
+    @Column(length = 100)
+    private String password;
+
+    private Integer status;
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
