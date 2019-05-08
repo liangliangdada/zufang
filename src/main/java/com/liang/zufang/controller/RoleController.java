f99ac48470d6c603baa1bd251e7c513b6deb2259
e <1216569087@qq.com>
2019-05-08 16:55:34 +0800

    角色

---
+++ src/main/java/com/liang/zufang/controller/RoleController.java
@@ -0,0 +1,14 @@
+package com.liang.zufang.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author liuliang
+ * @description 角色管理
+ * @date 2019/5/8
+ **/
+@Controller
+@RequestMapping("role")
+public class RoleController {
+}
